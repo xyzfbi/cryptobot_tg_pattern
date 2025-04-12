7ecb46cdd9d6bee745a3d172fde34f8bbae18b85
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-12 21:54:05 +0300

    Added fonts + recolor background, grid, candles!!1

--- src/data_to_jpg.py
+++ src/data_to_jpg.py
@@ -4,6 +4,7 @@ import pandas as pd
 from mplfinance.original_flavor import candlestick_ohlc
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
+from matplotlib import  font_manager as fm, rcParams
 
 def depict_candle_graph(data, symbol="BTCUSDT"):
     #преобразование в формат матплотлиба
@@ -14,16 +15,34 @@ def depict_candle_graph(data, symbol="BTCUSDT"):
     data_ohlc = data_ohlc.astype(float)
     print(data_ohlc)
 
+    #шрифт
+
+    '''
+    github_url = 'https://github.com/google/fonts/blob/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf'
+    font_url = github_url + '?raw=true'
+    response = requests.get(font_url)
+    with open('temp.ttf', 'wb') as f:
+        f.write(response.content)
+    f.close()
+    '''
+    prop = fm.FontProperties(fname='../fonts/Arimo-VariableFont_wght.ttf')
     #создание фигуры
     fig, ax = plt.subplots(figsize=(15, 7))
-    
+    ax.set_facecolor('#101014')
+
     #рисование свечей
-    candlestick_ohlc(ax, data_ohlc.values, width = 0.6, colorup = 'lime', colordown = 'crimson')
+    candlestick_ohlc(ax, data_ohlc.values, width = 0.6, colorup = '#20b26c', colordown = '#ef454a')
+
 
     #настройки осей
     ax.xaxis_date()
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-    plt.xticks(rotation = 0)
+    plt.xticks(rotation = 0, color = '#737376')
+    plt.yticks(rotation = 0, color = '#737376')
+    for tick in ax.get_xticklabels():
+        tick.set_fontproperties(prop)
+    for tick in ax.get_yticklabels():
+        tick.set_fontproperties(prop)
 
     #количество отметок по оси y
     ax.yaxis.set_major_locator(mticker.MaxNLocator(nbins=12))
@@ -35,15 +54,19 @@ def depict_candle_graph(data, symbol="BTCUSDT"):
     ax.set_xlim(min_date, max_date + delta)
 
     #заголовки и сетка
-    plt.title(symbol, fontsize = 18, fontweight='bold')
-    plt.xlabel('Дата и время', fontsize = 16, fontweight='bold')
-    plt.ylabel('Цена', fontsize = 16, fontweight='bold')
-    plt.grid(True, linestyle = '--', alpha = 0.5)
+    plt.title(symbol, fontsize = 24, fontweight='bold', color = '#737376', fontproperties = prop)
+    plt.xlabel('Date and time', fontsize = 14, fontweight='bold', color = '#737376', fontproperties = prop)
+    plt.ylabel('Price', fontsize = 14, fontweight='bold', color = '#737376', fontproperties = prop)
+    plt.grid(True, alpha=0.2, color='#202124', linewidth=2)
+    for spine in ax.spines.values():
+        spine.set_color('#71757a')
+        spine.set_linewidth(1)
+
 
     #цвет фона
-    fig.patch.set_facecolor('#f0f8ff')
+    fig.patch.set_facecolor('#101014')
 
-    
+
     plt.tight_layout()
     plt.show()
 
